cc8cf6c5d230103f4dfdd5a03d6463f456ccf42f
Hee Lup Chong <neonin@gmail.com>
2025-03-24 13:24:29 +0800

    Add project files.

---
+++ SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
@@ -0,0 +1,69 @@
+﻿namespace SimpleXMLValidatorLibrary
+{
+    public class SimpleXmlValidator
+    {
+        public static bool DetermineXml(string xml)
+        {
+            // Perform the basic check
+            if (!InitAndPerformBasicCheck(xml)) return false;
+            
+            // Local variables
+            Stack<string> tagStack = new Stack<string>();
+            bool isClosing = false;
+            string readString = string.Empty;
+
+            do
+            {
+                // If a tag...
+                if (xml[0].Equals('<'))
+                    xml = xml.Substring(1);
+                else
+                {
+                    // If data, discard it and continue
+                    xml = xml.Substring(xml.IndexOf("<"));
+                    continue;
+                }
+
+                // Read the tag
+                readString = xml.Substring(0, xml.IndexOf(">"));
+
+                // Check if the tag is valid
+                if (!ProcessTag(tagStack, ref isClosing, readString)) return false;
+
+                // Discard the processed string
+                xml = xml.Substring(readString.Length + 1);
+            }
+            while (xml.Length > 0);
+
+            return true;
+        }
+
+        private static bool ProcessTag(Stack<string> TagStack, ref bool isClosing, string readString)
+        {
+            // Invalidate if empty tag, or an opening tag after closing
+            if (readString.Length == 0 || (isClosing && !readString[0].Equals('/')))
+                return false;
+
+            // Check if closing tag and validate the tag
+            if (readString[0].Equals('/'))
+            {
+                isClosing = true;
+                return TagStack.Pop().Equals(readString.Substring(1));
+            }
+            else
+                // Push the tag to stack
+                TagStack.Push(readString);
+
+            return true;
+        }
+
+        private static bool InitAndPerformBasicCheck(string xml)
+        {
+            // Trim leading and trailing whitespaces
+            xml = xml.Trim();
+
+            // Check if the string starts with < and ends with > char.
+            return xml.StartsWith("<") && xml.EndsWith(">");
+        }
+    }
+}
